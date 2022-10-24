3a129caa60fba4f59bedbf386a67198035dc71ba
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-24 14:55:58 -0300

    feat: Mocks para teste unitário das camadas

--- src/tests/mocks/carMocks.ts
+++ src/tests/mocks/carMocks.ts
@@ -10,7 +10,7 @@ const carMock: ICar = {
   seatsQty: 5,
 };
 
-const carMockWithId: ICar & { _id: string} = {
+const carMockWithId: ICar & { _id: string } = {
   _id: '62cf1fc6498565d94eba52cd',
   model: 'Chevrolet',
   year: 2015,
@@ -21,4 +21,37 @@ const carMockWithId: ICar & { _id: string} = {
   seatsQty: 5,
 }
 
-export { carMock, carMockWithId };
+const allCarsMock: ICar[] & { _id: string }[] = [
+  {
+    _id: '62cf1fc6498565d94eba52cd',
+    model: 'Chevrolet',
+    year: 2015,
+    color: 'Prata',
+    status: true,
+    buyValue: 30000,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+  {
+    _id: '73cf1fc6498565d94eba52cd',
+    model: 'Chevrolet',
+    year: 2019,
+    color: 'Ônix',
+    status: true,
+    buyValue: 45000,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+  {
+    _id: '84cf1fc6498565d94eba52cd',
+    model: 'Prisma',
+    year: 2016,
+    color: 'Prata',
+    status: true,
+    buyValue: 35000,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+];
+
+export { carMock, carMockWithId, allCarsMock };
