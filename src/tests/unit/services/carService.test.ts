554a094a86370d9cb38218866e25a7099e79d8e1
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-24 14:56:51 -0300

    test: adiciona teste dos metodos read e readOne camada service de Car

--- src/tests/unit/services/carService.test.ts
+++ src/tests/unit/services/carService.test.ts
@@ -4,15 +4,18 @@ const { expect } = chai;
 import { Model } from 'mongoose';
 import CarModel from '../../../models/Car';
 import CarService from '../../../services/Car';
-import { carMock, carMockWithId } from '../../mocks/carMocks';
+import { carMock, carMockWithId, allCarsMock } from '../../mocks/carMocks';
 import { ZodError } from 'zod';
+import { ErrorTypes } from '../../../errors/catalog';
 
-describe('Car Model', () => {
+describe('Car Service', () => {
   const carModel = new CarModel();
   const carService = new CarService(carModel);
 
   before(async () => {
     sinon.stub(Model, 'create').resolves(carMockWithId);
+    sinon.stub(Model, 'find').onCall(0).resolves(allCarsMock).onCall(1).resolves([]);
+    sinon.stub(Model, 'findOne').onCall(0).resolves(carMockWithId).onCall(1).resolves(null);
   });
 
   after(()=>{
@@ -36,5 +39,38 @@ describe('Car Model', () => {
       expect(error).to.be.instanceOf(ZodError);
     })
   });
+
+  describe('searching all cars', () => {
+    it('successfully found', async () => {
+      const allCarsFound = await carService.read();
+
+      expect(allCarsFound).to.be.deep.equal(allCarsFound);
+    })
+
+    it('if dont have cars, returns an empty list', async () => {
+      const allCarsFound = await carService.read();
+
+      expect(allCarsFound).to.be.deep.equal([]);
+    })
+  })
+
+  describe('searching a car', () => {
+    it('successfully found', async () => {
+      const carFound = await carService.readOne(carMockWithId._id);
+
+      expect(carFound).to.be.deep.equal(carMockWithId);
+    })
+
+    it('_id not found', async () => {
+      let error;
+      try {
+        await carService.readOne(carMockWithId._id);
+      } catch (err: any) {
+        error = err.message
+      }
+
+      expect(error).to.be.deep.equal(ErrorTypes.EntityNotFound);
+    })
+  })
 
 });
