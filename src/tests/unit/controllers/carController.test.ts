ecf7b747f372f0555159bb9b7e8f6a5b78909ba3
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-24 10:56:59 -0300

    test: camada controller de Car

---
+++ src/tests/unit/controllers/carController.test.ts
@@ -0,0 +1,38 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import { NextFunction, Request, Response } from 'express';
+import CarModel from '../../../models/Car';
+import CarService from '../../../services/Car';
+import CarController from '../../../controllers/Car';
+import { carMock, carMockWithId } from '../../mocks/carMocks';
+
+describe('Car Model', () => {
+  const carModel = new CarModel();
+  const carService = new CarService(carModel);
+  const carController = new CarController(carService);
+  const req = {} as Request;
+  const res = {} as Response;
+
+  before(async () => {
+    sinon.stub(carService, 'create').resolves(carMock);
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+
+  describe('creating a car', () => {
+    it('successfully created', async () => {
+      req.body = carMock;
+      await carController.create(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+    });
+  });
+
+});
