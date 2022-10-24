f4d9bc105525efe77308055dd9ce545db02bb5bf
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-24 14:56:40 -0300

    test: adiciona teste dos metodos read e readOne camada model de Car

--- src/tests/unit/models/carModel.test.ts
+++ src/tests/unit/models/carModel.test.ts
@@ -3,13 +3,16 @@ import chai from 'chai';
 const { expect } = chai;
 import { Model } from 'mongoose';
 import CarModel from '../../../models/Car';
-import { carMock, carMockWithId } from '../../mocks/carMocks';
+import { carMock, carMockWithId, allCarsMock } from '../../mocks/carMocks';
+import { ErrorTypes } from '../../../errors/catalog';
 
 describe('Car Model', () => {
   const carModel = new CarModel();
 
   before(async () => {
     sinon.stub(Model, 'create').resolves(carMockWithId);
+    sinon.stub(Model, 'find').onCall(0).resolves(allCarsMock).onCall(1).resolves([]);
+    sinon.stub(Model, 'findOne').resolves(carMockWithId);
   });
 
   after(()=>{
@@ -22,5 +25,38 @@ describe('Car Model', () => {
       expect(newCar).to.be.deep.equal(carMockWithId);
     });
   });
+
+  describe('searching all cars', () => {
+    it('successfully found', async () => {
+      const allCarsFound = await carModel.read();
+
+      expect(allCarsFound).to.be.deep.equal(allCarsFound);
+    })
+
+    it('if dont have cars, returns an empty list', async () => {
+      const allCarsFound = await carModel.read();
+
+      expect(allCarsFound).to.be.deep.equal([]);
+    })
+  })
+
+  describe('searching a car', () => {
+    it('successfully found', async () => {
+      const carFound = await carModel.readOne(carMockWithId._id);
+
+      expect(carFound).to.be.deep.equal(carMockWithId);
+    })
+
+    it('_id invalid', async () => {
+      let error;
+      try {
+        await carModel.readOne('123ERRADO');
+      } catch (err: any) {
+        error = err.message
+      }
+
+      expect(error).to.be.deep.equal(ErrorTypes.InvalidMongoId);
+    })
+  })
 
 });
