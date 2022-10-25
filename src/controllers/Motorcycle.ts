abfd0cba7d696cb1f8f035611bafc24b023305b2
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-25 10:37:12 -0300

    feat: implementa todos os métodos conforme interface IService na camada de controller de Motorcycle

---
+++ src/controllers/Motorcycle.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import IService from '../interfaces/IService';
+import { IMotorcycle } from '../interfaces/IMotorcycle';
+
+class MotorcycleController {
+  constructor(private _service: IService<IMotorcycle>) {}
+  
+  public async create(req: Request, res: Response<IMotorcycle>) {
+    const results = await this._service.create(req.body);
+
+    return res.status(201).json(results);
+  }
+
+  public async read(req: Request, res: Response<IMotorcycle[]>) {
+    const results = await this._service.read();
+
+    return res.status(200).json(results);
+  }
+
+  public async readOne(req: Request, res: Response<IMotorcycle>) {
+    const result = await this._service.readOne(req.params.id);
+
+    return res.status(200).json(result);
+  }
+
+  public async update(req: Request, res: Response<IMotorcycle>) {
+    const result = await this._service.update(req.params.id, req.body);
+
+    return res.status(200).json(result);
+  }
+
+  public async delete(req: Request, res: Response<IMotorcycle>) {
+    await this._service.delete(req.params.id);
+
+    return res.status(204).end();
+  }
+}
+
+export default MotorcycleController;
