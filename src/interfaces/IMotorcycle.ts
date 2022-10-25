b0a08ef41f768d765fbc6766f04258b53cabf340
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-25 10:35:53 -0300

    fix: adiciona propriedade "positive" ao Schema

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -5,7 +5,7 @@ const CATEGORY = ['Street', 'Custom', 'Trail'] as const;
 
 const MotorcycleZodSchema = VehicleZodSchema.extend({
   category: z.enum(CATEGORY),
-  engineCapacity: z.number().max(2500),
+  engineCapacity: z.number().positive().max(2500),
 });
 
 export type IMotorcycle = z.infer<typeof MotorcycleZodSchema>;
