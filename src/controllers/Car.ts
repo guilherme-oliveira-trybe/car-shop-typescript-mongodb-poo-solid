448ae3f6acfa655723716d30160392834b9a684a
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-25 09:31:57 -0300

    feat: método delete na camada de controller de car

--- src/controllers/Car.ts
+++ src/controllers/Car.ts
@@ -28,6 +28,12 @@ class CarController {
 
     return res.status(200).json(result);
   }
+
+  public async delete(req: Request, res: Response<ICar>) {
+    await this._service.delete(req.params.id);
+
+    return res.status(204).end();
+  }
 }
 
 export default CarController;
